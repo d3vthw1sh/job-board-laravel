cbc2c4d510503721db6335ccb3959271aefb813e
faint <Vichet.sopheaktra.work@gmail.com>
2025-06-08 03:08:51 +0700

    Force HTTPS asset URLs in Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+  base: 'https://jobpadoy-55dmz.ondigitalocean.app/build/', // <--- FORCE HTTPS HERE!
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
